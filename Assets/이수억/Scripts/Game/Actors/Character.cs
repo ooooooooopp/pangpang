dc17b49a6456325d7ef405035e44e921b770e802
suoki <esock@naver.com>
2020-07-25 01:31:31 +0900

    DoTween 추가. ConsolePro 추가. 풀링 적용

---
+++ Assets/이수억/Scripts/Game/Actors/Character.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//플레이어 스탯 있을예정인것.
+//공격력 공격속도 체력 이동속도
+
+public class Character : Actor
+{
+	public float moveSpeed = 4f;
+	private float moveDir = 0f;
+	public Rigidbody2D rb;
+
+	void Update()
+	{
+		moveDir = Input.GetAxisRaw( "Horizontal" );
+		CheckFire();
+	}
+
+	void CheckFire()
+	{
+		if ( Input.GetKeyDown( KeyCode.Space ) ) {
+			var bullet = PoolFactory.In.GenerateBullet( "Chain", StageMan.In.con.holder.bullets );
+			bullet.Activate( position );
+		}
+	}
+
+	void FixedUpdate()
+	{
+		rb.MovePosition( rb.position + new Vector2( moveDir * Time.fixedDeltaTime * moveSpeed, 0f ) );
+	}
+
+	void OnCollisionEnter2D( Collision2D col )
+	{
+		if ( col.collider.tag == "Ball" ) {
+			PoolMgr.In.Destory();
+			//Debug.Log( "GAME OVER!" );
+			SceneManager.LoadScene( SceneManager.GetActiveScene().buildIndex );
+		}
+	}
+}
