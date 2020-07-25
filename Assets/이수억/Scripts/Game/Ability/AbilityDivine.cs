9b166aee498b3c7eda30c6612f167f4b0473d114
suoki <esock@naver.com>
2020-07-25 19:25:27 +0900

    Play 씬 재수정

---
+++ Assets/이수억/Scripts/Game/Ability/AbilityDivine.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityDivine : Ability
+{
+	public bool isOn = false;
+
+	public void Activate()
+	{
+		if ( isOn == false ) {
+			InvokeRepeating( "Divine", 0f, Balance.HEALING_TERM );
+			isOn = true;
+		}
+	}
+
+	public void DeActivate()
+	{
+		isOn = false;
+		CancelInvoke( "Divine" );
+	}
+
+	void Divine()
+	{
+		if ( c == null )
+			return;
+
+		//c.stat.Heal( c.stat.healing );
+		//Broadcaster.SendEvent( Constant.Event.RefreshUI, TypeOfMessage.dontRequireReceiver );
+	}
+}
