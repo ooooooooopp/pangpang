7f5f8066108c902c2f0990b949dd1cfce65d2d55
suoki <esock@naver.com>
2020-07-25 14:37:35 +0900

    어빌리티 획득창 추가.

---
+++ Assets/이수억/Scripts/Game/UI/UIAbility.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class UIAbility : MonoBehaviour
+    {
+        public Button button;
+        public Image image;
+        public Text txtName;
+
+        AbilityKind kind;
+        Character c;
+
+        public void Init(AbilityKind kind, Character c)
+        {
+            this.kind = kind;
+            this.c = c;
+            txtName.text = kind.ToString();
+        }
+
+        public void OnGetAbility()
+        {
+            c.abilityCon.AddAbility( kind );
+            button.interactable = false;
+            image.color = Color.green;
+        }
+    }
+}
---
+++ Assets/이수억/Scripts/Game/UI/UIAbilityTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI
+{
+	public class UIAbilityTest : MonoBehaviour
+	{
+		public GameObject pfAbility;
+		public Transform holder;
+
+		public void Init()
+		{
+			GenerateStates();
+		}
+
+		public void GenerateStates()
+		{
+			var abilites = System.Enum.GetValues( typeof( AbilityKind ) );
+			foreach ( var item in abilites ) {
+				AbilityKind abil = (AbilityKind)item;
+				if ( abil == AbilityKind.None )
+					continue;
+
+				var obj = Instantiate( pfAbility, holder ).GetComponent<UIAbility>();
+				obj.Init( abil, StageMan.In.player );
+			}
+		}
+	}
+
+}
