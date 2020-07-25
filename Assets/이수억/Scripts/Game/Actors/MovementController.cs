52b059f8625483040c6642646cfed4e685fb602c
suoki <esock@naver.com>
2020-07-26 03:38:26 +0900

    대쉬 추가.

--- Assets/이수억/Scripts/Game/Actors/MovementController.cs
+++ Assets/이수억/Scripts/Game/Actors/MovementController.cs
@@ -1,4 +1,5 @@
-﻿using MoreMountains.Tools;
+﻿using DG.Tweening;
+using MoreMountains.Tools;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -12,6 +13,7 @@ public enum PlayerState
 	die,
 	hit,
 	jump,
+	dash,
 }
 
 public class MovementController : PlayerController
@@ -21,10 +23,15 @@ public class MovementController : PlayerController
 	public Rigidbody2D rb;
 
 	private float moveDir = 0f;
-	float beforeInput = 0f;
 
-	//float noInputTerm = 0f;
+	private const float DOUBLE_CLICK_TIME = 1f;
+	private float lastClickTime;
+	
+	float beforeDir;
 
+	float dashCoolTime = .5f;
+	float curDashDelay = 0f;
+	bool isDashPossible => curDashDelay >= dashCoolTime;
 
 	public override void Init( Character c )
 	{
@@ -39,36 +46,55 @@ public class MovementController : PlayerController
 		if ( c.stat.isDie )
 			return;
 
-		if ( state.CurrentState == PlayerState.attkck || state.CurrentState == PlayerState.die || state.CurrentState == PlayerState.hit ) {
+		if ( state.CurrentState == PlayerState.attkck || state.CurrentState == PlayerState.die || state.CurrentState == PlayerState.hit || state.CurrentState == PlayerState.dash ) {
 			return;
 		}
 
+		curDashDelay += Time.deltaTime;
+
 		moveDir = Input.GetAxisRaw( "Horizontal" );
-
-
-		if ( moveDir >= 1f ) {
-			c.render.Right();
-			Transition( PlayerState.move, false );
-		}
-		else if( moveDir <= -1f ){
-			c.render.Left();
-			Transition( PlayerState.move, false );
-		} else {
+		if( moveDir == 0 ) {
 			Transition( PlayerState.idle, false );
-
+			return;
 		}
 
-		beforeInput = moveDir;
+		CheckFlip( moveDir );
 
 		//Log.Check.I( $"[MoveDir] {moveDir}" );
+		if ( isDashPossible && beforeDir == moveDir && state.CurrentState == PlayerState.idle ) {
+			float timeSinceLastClick = Time.time - lastClickTime;
+			if ( timeSinceLastClick <= DOUBLE_CLICK_TIME ) {
+				// Double click!
+				Transition( PlayerState.dash, true );
+			} else {
+				// Normal click!
+				Transition( PlayerState.move, false );
+			}
+			lastClickTime = Time.time;
+		} else {
+			Transition( PlayerState.move, false );
+		}
+		beforeDir = moveDir;
 	}
+
+
+	void CheckFlip(float dir )
+	{
+		if ( dir >= 1f ) {
+			c.render.Right();
+		} else if ( dir <= -1f ) {
+			c.render.Left();
+		} else {
+			
+		}
+	}
+
 
 	void FixedUpdate()
 	{
-		if ( state.CurrentState == PlayerState.attkck || state.CurrentState == PlayerState.die || state.CurrentState == PlayerState.hit ) {
+		if ( state.CurrentState == PlayerState.attkck || state.CurrentState == PlayerState.die || state.CurrentState == PlayerState.hit || state.CurrentState == PlayerState.dash) {
 			return;
 		}
-
 		rb.MovePosition( rb.position + new Vector2( moveDir * Time.fixedDeltaTime * c.stat.movSpd, 0f ) );
 	}
 
@@ -76,7 +102,7 @@ public class MovementController : PlayerController
 
 	public void Transition(PlayerState targetState, bool forceChange = false )
 	{
-		//Log.Check.I( $"[Transition] Try {targetState}" );
+		Log.Check.I( $"[Transition] Try {targetState}" );
 
 		if ( targetState == state.CurrentState && forceChange == false)
 			return;
@@ -113,11 +139,38 @@ public class MovementController : PlayerController
 				Invoke( "OnHitEnd", 0.15f );
 
 				break;
+			case PlayerState.dash:
+				c.render.PlayAnimation( targetState, true, null, 0.1f );
+
+				float minX = -8f;
+				float maxX = 8f;
+				float target = 3f;
+				Vector3 dashFxRot = Vector3.zero;
+				if( moveDir == 1f ) {
+					target = Mathf.Min( Mathf.Abs(maxX - c.position.x), target );
+					dashFxRot = new Vector3( -180f, 90f, 180f );
+				} else if( moveDir == -1f ) {
+					target = Mathf.Min( Mathf.Abs( minX  - c.position.x), target );
+					target *= -1f;
+					dashFxRot = new Vector3( -180f, -90f, 180f );
+				}
+				Vector3 targetPos = c.position + new Vector3( target, 0, 0 );
+				c.transform.DOMove( targetPos, 0.25f).SetEase( Ease.OutCubic ).OnComplete( () => {
+					Transition( PlayerState.idle );
+				} );
+				curDashDelay = 0f;
+
+				var fx = PoolFactory.In.GenerateFx( "DashFx", StageMan.In.con.holder.bulletHolder );
+				fx.Activate( c.body.position + new Vector3( moveDir * 2f, -0.2f, 0 ) );
+				fx.rotation = Quaternion.Euler( dashFxRot.x, dashFxRot.y, dashFxRot.z );
+
+
+				break;
 			default:
 				break;
 		}
 
-		//Log.Check.I( $"[Transition] - Success {targetState}" );
+		Log.Check.I( $"[Transition] - Success {targetState}" );
 		state.ChangeState( targetState );
 	}
 
@@ -125,6 +178,26 @@ public class MovementController : PlayerController
 	//{
 	//	yield return new WaitForSeconds( time );
 	//	action?.Invoke();
+	//}
+
+	//Coroutine dashCo = null;
+	//IEnumerator DashSpdDownCo()
+	//{
+	//	dashSpeed = 50f;
+	//	yield return new WaitForSeconds( 0.1f );
+	//	dashSpeed = 1f;
+	//	dashCo = null;
+	//	Transition( PlayerState.idle );
+	//	//while ( true ) {
+	//	//	yield return null;
+	//	//	dashSpeed -= Time.deltaTime * 20f;
+	//	//	if( dashSpeed <= 1f ) {
+	//	//		dashSpeed = 1f;
+	//	//		dashCo = null;
+	//	//		Transition( PlayerState.idle );
+	//	//		yield break;
+	//	//	}
+	//	//}
 	//}
 
 	void OnHitEnd()
@@ -143,5 +216,5 @@ public class MovementController : PlayerController
 	{
 		c.OnDie();
 	}
-
 }
+
